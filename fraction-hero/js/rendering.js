ee5f6896c59c2c53d1e163029fe2b66a5313b0f6
Joe Zeng <joe@joezeng.com>
2016-03-08 22:44:00 -0500

    Update rendering.js

--- fraction-hero/js/rendering.js
+++ fraction-hero/js/rendering.js
@@ -7,8 +7,8 @@ function setup_rendering() {
 	var width = 256, height = 1152;
 	renderer = PIXI.autoDetectRenderer(width, height,
 		{transparent: true, resolution: window.devicePixelRatio});
-	renderer.view.style.width = width;
-	renderer.view.style.height = height;
+	renderer.view.style.width = width * 4;
+	renderer.view.style.height = height * 4;
 	renderer.view.style.border = "1px solid white";
 
 	gameplay_stage = new PIXI.Container();
